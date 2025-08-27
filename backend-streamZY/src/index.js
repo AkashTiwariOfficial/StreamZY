8be0056b17a370ed31dca525d33c3949d21e5ee8
AKashTiwariOfficial <akashtiwari00624@gmail.com>
2025-08-27 23:38:40 +0530

    ... still working

---
+++ backend-streamZY/src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './env'
+});
+
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`StreamZY is running on port: ${process.env.PORT}`);
+    }) 
+})
+.catch((error) => {
+    console.log("MongoDb connection failed !!", error);
+})
+
